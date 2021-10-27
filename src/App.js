3967be1dbcea294ee56318990f98e7718be84a30
Suheyla Cetin <Suheyla@mehmets-MacBook-Pro.local>
2021-10-26 18:21:55 -0700

    simple counter-react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount(count => count + 1)
+  }
+  const handleDecrement = () => {
+    setCount(count => count - 1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Count Anything!</h1>
+     <p>{count}</p>
+     <button onClick={handleIncrement}>+</button>
+     <button onClick={handleDecrement}>-</button>
     </div>
   );
 }
